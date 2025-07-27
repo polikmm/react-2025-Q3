4158ab154b4d7003e5a0028a5a5a9b35bf068a50
polikmm <polikmm@mail.ru>
2025-07-27 10:59:30 +0400

    refactor: add errorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import Main from './components/Main/Main';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Author from './components/Author/Author';
 import Details from './components/Details/Details';
+import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/page/1" replace />} />
-        <Route path="page/:page" element={<Main />}>
-          <Route index element={<div />} />
-          <Route path="details/:id" element={<Details />} />
-        </Route>
-        <Route path="/about" element={<Author />} />
-      </Routes>
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" element={<Navigate to="/page/1" replace />} />
+          <Route path="page/:page" element={<Main />}>
+            <Route index element={<div />} />
+            <Route path="details/:id" element={<Details />} />
+          </Route>
+          <Route path="/about" element={<Author />} />
+        </Routes>
+      </ErrorBoundary>
     </BrowserRouter>
   );
 }
